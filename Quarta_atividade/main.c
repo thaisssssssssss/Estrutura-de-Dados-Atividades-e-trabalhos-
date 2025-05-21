725187dc330574315442d574bd06d432395ddf01
thaisssssssssss <thaisgomes1585@gmail.com>
2025-05-21 16:55:08 -0300

    atualizacao pra receber mais produtos

--- Quarta_atividade/main.c
+++ Quarta_atividade/main.c
@@ -12,25 +12,32 @@ char* lerLinha(){
 }
 
 int main(){
-    char* nome;
-    int valor, codigo;
-    
-    printf("Insira o nome do produto:\n");
-    nome = lerLinha();
-    
-    printf("Insira o valor do produto:\n");
-    scanf("%d", &valor);
-    
-    printf("Insira o codigo do produto:\n");
-    scanf("%d", &codigo);
-
-    tProduto* prod1 = criaProduto(nome, valor, codigo);
-
+    int qtdProd, i;
+    scanf("%d", &qtdProd);
 
     Lista* lista = criaLista();
-    insereProdutoLista(lista, prod1);
-    //tProduto* p = retiraProdutoLista(lista, 1234);
+    
+    for(i = 0; i < qtdProd; i++){
+        char* nome;
+        int valor, codigo;
+        
+        printf("Insira o nome do produto:\n");
+        nome = lerLinha();
+        
+        printf("Insira o valor do produto:\n");
+        scanf("%d", &valor);
+        
+        printf("Insira o codigo do produto:\n");
+        scanf("%d", &codigo);
+    
+        tProduto* prod1 = criaProduto(nome, valor, codigo);
+        insereProdutoLista(lista, prod1);
+
+    }
+
+
     liberaLista(lista);
+    //tProduto* p = retiraProdutoLista(lista, 1234);
     //imprimeProduto(p);
     //imprimeLista(lista);
 }
